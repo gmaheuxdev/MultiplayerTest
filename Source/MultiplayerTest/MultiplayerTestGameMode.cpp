f05e4a9df80063c1bebdec1d307d22eab6066f70
Ghyslain Maheux <gmaheuxdev@outlook.com>
2018-02-28 13:40:40 -0500

    Initial commit

---
+++ Source/MultiplayerTest/MultiplayerTestGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "MultiplayerTestGameMode.h"
+#include "MultiplayerTestCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AMultiplayerTestGameMode::AMultiplayerTestGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
